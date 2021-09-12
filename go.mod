b2e773f9524fb2600fca8fe52754198a1077f520
Kai Hendry <hendry@iki.fi>
2021-09-12 09:05:22 +0800

    Web-ify

--- go.mod
+++ go.mod
@@ -1,3 +1,13 @@
 module example.com/test
 
 go 1.17
+
+require (
+	github.com/apex/gateway v1.1.2
+	github.com/apex/log v1.9.0
+)
+
+require (
+	github.com/aws/aws-lambda-go v1.17.0 // indirect
+	github.com/pkg/errors v0.9.1 // indirect
+)
--- main.go
+++ main.go
@@ -1,19 +1,63 @@
 package main
 
 import (
-	"flag"
-	"fmt"
+	"embed"
+	"html/template"
+	"net/http"
+	"os"
 	"time"
+
+	"github.com/apex/gateway"
+	"github.com/apex/log"
+	jsonhandler "github.com/apex/log/handlers/json"
+	"github.com/apex/log/handlers/text"
 )
 
-func main() {
-	flag.Parse()
-	t, err := time.Parse("2006-01", flag.Args()[0])
-	if err != nil {
-		panic(err)
-	}
-	monthEnd := t.AddDate(0, 1, -1) // add a month, minus a day
+//go:embed templates
+var tmpl embed.FS
+var Version string
+
+func days(month time.Time) (days []time.Time) {
+	monthEnd := month.AddDate(0, 1, -1) // add a month, minus a day
 	for i := 0; i < monthEnd.Day(); i++ {
-		fmt.Println(t.AddDate(0, 0, i).Format("2006-01-02 Mon"))
+		days = append(days, month.AddDate(0, 0, i))
 	}
+	return
+}
+
+func main() {
+	t, err := template.ParseFS(tmpl, "templates/*.html")
+	if err != nil {
+		log.WithError(err).Fatal("Failed to parse templates")
+	}
+
+	http.HandleFunc("/", func(rw http.ResponseWriter, r *http.Request) {
+		chosenDate, err := time.Parse("2006-01", r.URL.Query().Get("month"))
+		if err != nil {
+			log.Warn("bad input, defaulting to current month")
+			chosenDate = time.Now()
+		}
+		rw.Header().Set("Content-Type", "text/html")
+		err = t.ExecuteTemplate(rw, "index.html", struct {
+			Month  time.Time
+			Days   []time.Time
+			Time   time.Time
+			Header http.Header
+		}{chosenDate, days(chosenDate), time.Now(), r.Header})
+		if err != nil {
+			http.Error(rw, err.Error(), http.StatusInternalServerError)
+			log.WithError(err).Fatal("Failed to execute templates")
+		}
+	})
+
+	port := os.Getenv("_LAMBDA_SERVER_PORT")
+	if port == "" {
+		log.SetHandler(text.Default)
+		err = http.ListenAndServe(":"+os.Getenv("PORT"), nil)
+	} else {
+		log.SetHandler(jsonhandler.Default)
+		err = gateway.ListenAndServe("", nil)
+	}
+	log.Fatalf("failed to start server: %v", err)
+
 }
